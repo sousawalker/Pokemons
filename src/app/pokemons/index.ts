53e5985a3be19d2e9913739d68b65b455da5cdf4
Walker Sousa <sousa.walker@gmail.com>
2018-04-27 18:25:42 -0300

    Adição de component Pokemnons.

---
+++ src/app/pokemons/index.ts
@@ -0,0 +1,4 @@
+export * from './pokemons.component';
+
+export * from './list';
+export * from './details';
---
+++ src/app/pokemons/pokemons.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ListService } from '../shared';
+
+@Component({
+  selector: 'poke-pokemons',
+  templateUrl: './pokemons.component.html',
+  styleUrls: ['./pokemons.component.scss']
+})
+export class PokemonsComponent implements OnInit {
+  pokeList;
+  count;
+
+  prev;
+  next;
+
+  sub;
+  pag;
+
+  constructor( private activatedRoute: ActivatedRoute, private list: ListService ) { }
+
+  ngOnInit() {
+    this.sub = this.activatedRoute.params.subscribe(params => {
+      this.pag = params['pag'];
+
+      this.list.pokeList(this.pag).subscribe((data) => {
+        let response = data.json();
+
+        this.prev = response.previous;
+        this.next = response.next;
+
+        this.pokeList = response.results;
+      }, (error) => {
+        console.log(error);
+      })
+    });
+  }
+}
