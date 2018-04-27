1be6c4b7be51d05944482d100176d9f89a8041b3
Walker Sousa <sousa.walker@gmail.com>
2018-04-27 18:53:22 -0300

    Correção de pipe.

--- src/app/shared/pipes/pag/pag.pipe.ts
+++ src/app/shared/pipes/pag/pag.pipe.ts
@@ -7,7 +7,11 @@ export class PagPipe implements PipeTransform {
 
   transform(value: any, args?: any): any {
     let pag = value.replace('https://pokeapi.co/api/v2/pokemon/?limit=20&offset=', '');
-    
-    return pag/20;
+
+    if(pag > 0) {
+      return pag/20;
+    }else{
+      return "";
+    }
   }
 }
