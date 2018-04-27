76e4dee0427e4f33b8266721b3d302b57902e867
Walker Sousa <sousa.walker@gmail.com>
2018-04-27 18:22:14 -0300

    Adição de components, pipres, services e models.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,45 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpModule } from '@angular/http';
 import { AppRoutingModule } from './app-routing.module';
-
 import { AppComponent } from './app.component';
 
+import { PokemonsComponent } from './pokemons/pokemons.component';
+import { ListComponent } from './pokemons/list/list.component';
+import { DetailsComponent } from './pokemons/details/details.component';
+
+import {
+  DetailsService,
+  ListService,
+  ConcatIdPipe,
+  UcwordsPipe,
+  FooterComponent,
+  PaginationComponent,
+  PagPipe
+} from './shared';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PokemonsComponent,
+    ListComponent,
+    DetailsComponent,
+    ConcatIdPipe,
+    UcwordsPipe,
+    FooterComponent,
+    PaginationComponent,
+    PagPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [
+    DetailsService,
+    ListService,
+    PagPipe
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
