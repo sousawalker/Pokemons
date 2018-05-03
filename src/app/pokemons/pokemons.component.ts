9b3bdfbf8a2032d639e9ac038d0db7bf44816634
Walker Sousa <sousa.walker@gmail.com>
2018-05-03 16:01:12 -0300

    Gravação de página local storage.

--- src/app/pokemons/pokemons.component.ts
+++ src/app/pokemons/pokemons.component.ts
@@ -23,6 +23,8 @@ export class PokemonsComponent implements OnInit {
     this.sub = this.activatedRoute.params.subscribe(params => {
       this.pag = params['pag'];
 
+      localStorage.setItem('pag', this.pag);
+
       this.list.pokeList(this.pag).subscribe((data) => {
         let response = data.json();
 
